321d606a616e36b12e2a13608fa46b934b70de25
DaviFaustino <davifaustino.dev@gmail.com>
2025-06-20 16:19:01 -0300

    feat: add background and header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup>
-
+import logo from '/src/assets/logo.svg'
 </script>
 
 <template>
-
+    <header class="flex justify-center">
+        <img :src="logo" alt="Logo" class="w-72 sm:w-96 h-auto mt-15"/>
+    </header>
 </template>
-
-<style scoped>
-
-</style>
