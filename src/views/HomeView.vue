5464cafe283e7a4d646d620db2a7c5910f29eaed
DaviFaustino <davifaustino.dev@gmail.com>
2025-06-25 15:26:37 -0300

    feat: create dynamics input

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { computed, ref } from 'vue';
 import SearchTypeButton from '../components/SearchTypeButton.vue';
+import arrowicon from '/src/assets/arrow.svg';
 import mglassicon from '/src/assets/ma-glass-icon.svg';
 
 const byThemeOn = ref(true);
@@ -12,15 +13,57 @@ function showOption(optionId) {
     byThemeOn.value = !byThemeOn.value;
 }
 
+const dynamicTrackOn = ref(false);
+const knobPosition = ref(15);
+const knobX = computed(() => { return (knobPosition.value) / 32 * 100 });
+
 const searchInputPlaceHolder = computed(() => {
     return byThemeOn.value ? "Insira um tema" : "Insira um título";
 });
 const searchInput = ref("");
+const isSearchInputFull = computed(() => {
+    knobPosition.value = 15
+    return searchInput.value != "";
+});
 
 function onInputChange(event) {
     if (event.target.value[0] === " ") {
         searchInput.value = "";
     };
+}
+
+function positionKnob(event) {
+    const trackRect = document.querySelector('#track').getBoundingClientRect();
+    const trackMouseX = event.clientX - trackRect.left;
+    let knobWidth;
+
+    if (isSearchInputFull.value) {
+        knobWidth = trackRect.width == 320 ? 40: 48
+    } else {
+        knobWidth = trackRect.width == 320 ? 10: 12
+    }
+
+    if (trackMouseX < (knobWidth / 2)) {
+        knobPosition.value = 0;
+    } else {
+        let dynamicRange = isSearchInputFull.value ? 28 : 31;
+        if (trackMouseX > trackRect.width - (knobWidth / 2)) {
+            knobPosition.value = dynamicRange;
+        } else {
+            knobPosition.value = Math.round(((trackMouseX - (knobWidth / 2)) / (trackRect.width - knobWidth)) * dynamicRange);
+        }
+    }
+}
+
+function stopDragging() {
+    window.removeEventListener('mousemove', positionKnob);
+    window.removeEventListener('mouseup', stopDragging);
+}
+
+function startDragging(event) {
+    positionKnob(event);
+    window.addEventListener('mousemove', positionKnob);
+    window.addEventListener('mouseup', stopDragging);
 }
 </script>
 
@@ -34,7 +77,18 @@ function onInputChange(event) {
         <div class="flex flex-col items-center mt-10">
             <input type="text" :placeholder="searchInputPlaceHolder" v-model="searchInput" class="border-2 border-[#5D00F5] rounded-lg p-2 w-[20rem] sm:w-96" @input="onInputChange">
 
-            <button class="flex items-center border-1 border-[#5D00F5] rounded-lg py-1 px-2 space-x-2" :class="[ dynamicBarOn || !byThemeOn ? 'mt-8': 'mt-4']">
+            <div v-if="byThemeOn" class=" w-[20rem] sm:w-96">
+                <button class="flex items-center space-x-2 mb-1 text-lg" @click="dynamicTrackOn = !dynamicTrackOn">
+                    <img :src="arrowicon" alt="glass" class="w-5 h-7 my-1 ml-2" :class="[ dynamicTrackOn ? 'rotate-180' : '' ]"/>
+                    <div class="text-[#5D00F5]" v-if="!dynamicTrackOn">definir dinâmica</div>
+                </button>
+
+                <div id="track" class="relative w-[20rem] sm:w-96 h-4 rounded-2xl top-1/2 -translate-y-1/2 bg-cover bg-center" style="background: linear-gradient(90deg,rgba(0, 188, 250, 1) 0%, rgba(93, 0, 245, 1) 50%, rgba(245, 1, 6, 1) 100%);" v-if="dynamicTrackOn" @mousedown="startDragging">
+                    <div class="absolute h-5 border-4 border-gray-700 rounded-2xl top-1/2 -translate-y-1/2 pointer-events-none" :class="[isSearchInputFull ? 'w-10 sm:w-12' : 'w-[10px] sm:w-3' ]" :style="{left: knobX + '%'}"></div>
+                </div>
+            </div>
+
+            <button class="flex items-center border-1 border-[#5D00F5] rounded-lg py-1 px-2 space-x-2" :class="[ dynamicTrackOn || !byThemeOn ? 'mt-8': 'mt-4']">
                 <img :src="mglassicon" alt="glass" class="size-5" draggable="false">
                 <div class="text-lg text-[#5D00F5]">Buscar</div>
             </button>
