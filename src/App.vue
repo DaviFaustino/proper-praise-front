b7fd16709fb0a243f5e67bf97ec6902bc1c851e2
DaviFaustino <davifaustino.dev@gmail.com>
2025-07-04 17:32:47 -0300

    feat: create basic footer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import logo from '/src/assets/logo.svg'
-import HomeView from './views/HomeView.vue';
 </script>
 
 <template>
@@ -8,7 +7,15 @@ import HomeView from './views/HomeView.vue';
         <img :src="logo" alt="Logo" class="w-72 sm:w-96 h-auto mt-15"/>
     </header>
     
-    <main class="flex flex-col items-center justify-center">
-        <HomeView />
+    <main class="flex flex-col items-center flex-grow">
+        <RouterView />
     </main>
+
+    <footer class="flex flex-col h-20 bg-[#5D00F5] justify-center items-center">
+        <span class="text-white font-bold text-xl mb-2">KDLouvor</span>
+        <span class="text-white text-sm">
+            <RouterLink class="font-bold hover:underline" to="/terms-of-use">Termos de Uso</RouterLink> |
+            <RouterLink class="font-bold hover:underline" to="/terms-of-use">Política de Privacidade</RouterLink>
+        </span>
+    </footer>
 </template>
