6b0c2ef8ac0e93b17efdb68ee59a7ec5fec7d2be
DaviFaustino <davifaustino.dev@gmail.com>
2025-08-15 19:14:59 -0300

    feat: add terms of use

--- src/views/TermsOfUseView.vue
+++ src/views/TermsOfUseView.vue
@@ -1,3 +1,158 @@
 <template>
-    <div></div>
+    <div class="my-10 sm:my-10 lg:my-15 mx-5 sm:mx-10 md:mx-20 lg:mx-32 py-5 md:py-10 px-10 md:px-15 lg:px-32 bg-white">
+        <h2 class="text-2xl font-bold text-[#5D00F5]">
+            Termos e Condições
+        </h2>
+        <br>
+        <h3 class="text-lg font-bold text-[#5D00F5]">
+            <span>1. Termos</span>
+        </h3>
+        <br>
+        <p class="text-[#5D00F5]">
+            <span>
+                Ao acessar ao site <a class="font-bold hover:underline" href="https://kdlouvor.com">KDLouvor</a>,
+                concorda em cumprir estes termos de serviço, todas as leis e regulamentos aplicáveis ​​e concorda que é
+                responsável pelo cumprimento de todas as leis locais aplicáveis. Se você não concordar com algum desses
+                termos, está proibido de usar ou acessar este site. Os materiais contidos neste site são protegidos
+                pelas leis de direitos autorais e marcas comerciais aplicáveis.
+            </span>
+        </p>
+        <br>
+        <h3 class="text-lg font-bold text-[#5D00F5]">
+            <span>2. Uso de Licença</span>
+        </h3>
+        <br>
+        <p class="text-[#5D00F5]">
+            <span>
+                É concedida permissão para baixar temporariamente uma cópia dos
+                materiais (informações ou software) no site KDLouvor , apenas para visualização transitória pessoal e
+                não comercial. Esta é a concessão de uma licença, não uma transferência de título e, sob esta licença,
+                você não pode:&nbsp;
+            </span>
+        </p>
+        <br>
+        <ol class="list-decimal list-outside text-[#5D00F5] mx-8">
+            <li>
+                <span>modificar ou copiar os materiais;&nbsp;</span>
+            </li>
+            <li>
+                <span>
+                    usar os materiais para qualquer finalidade comercial ou para
+                    exibição pública (comercial ou não comercial);&nbsp;</span>
+                </li>
+            <li>
+                <span>
+                    tentar descompilar ou fazer engenharia reversa de qualquer
+                    software contido no site KDLouvor;&nbsp;</span>
+                </li>
+            <li>
+                <span>
+                    remover quaisquer direitos autorais ou outras notações de
+                    propriedade dos materiais; ou&nbsp;</span>
+                </li>
+            <li>
+                <span>
+                    transferir os materiais para outra pessoa ou 'espelhe' os
+                    materiais em qualquer outro servidor.
+                </span>
+            </li>
+        </ol>
+        <br>
+        <p class="text-[#5D00F5]">
+            <span>
+                Esta licença será automaticamente rescindida se você violar alguma
+                dessas restrições e poderá ser rescindida por KDLouvor a qualquer momento. Ao encerrar a visualização
+                desses materiais ou após o término desta licença, você deve apagar todos os materiais baixados em sua
+                posse, seja em formato eletrónico ou impresso.
+            </span>
+        </p>
+        <br>
+        <h3 class="text-lg font-bold text-[#5D00F5]">
+            <span>3. Isenção de responsabilidade</span>
+        </h3>
+        <br>
+        <ol class="list-decimal list-outside text-[#5D00F5] mx-8">
+            <li>
+                <span>
+                    Os materiais no site da KDLouvor são fornecidos 'como estão'.
+                    KDLouvor não oferece garantias, expressas ou implícitas, e, por este meio, isenta e nega todas as
+                    outras garantias, incluindo, sem limitação, garantias implícitas ou condições de comercialização,
+                    adequação a um fim específico ou não violação de propriedade intelectual ou outra violação de
+                    direitos.
+                </span>
+            </li>
+            <li>
+                <span>
+                    Além disso, o KDLouvor não garante ou faz qualquer representação
+                    relativa à precisão, aos resultados prováveis ​​ou à confiabilidade do uso dos materiais em seu site
+                    ou de outra forma relacionado a esses materiais ou em sites vinculados a este site.
+                </span>
+            </li>
+        </ol>
+        <br>
+        <h3 class="text-lg font-bold text-[#5D00F5]">
+            <span>4. Limitações</span>
+        </h3>
+        <br>
+        <p class="text-[#5D00F5]">
+            <span>
+                Em nenhum caso o KDLouvor ou seus fornecedores serão responsáveis ​​por
+                quaisquer danos (incluindo, sem limitação, danos por perda de dados ou lucro ou devido a interrupção dos
+                negócios) decorrentes do uso ou da incapacidade de usar os materiais em KDLouvor, mesmo que KDLouvor ou
+                um representante autorizado da KDLouvor tenha sido notificado oralmente ou por escrito da possibilidade
+                de tais danos. Como algumas jurisdições não permitem limitações em garantias implícitas, ou limitações
+                de responsabilidade por danos conseqüentes ou incidentais, essas limitações podem não se aplicar a
+                você.
+            </span>
+        </p>
+        <br>
+        <h3 class="text-lg font-bold text-[#5D00F5]">
+            <span>5. Precisão dos materiais</span>
+        </h3>
+        <br>
+        <p class="text-[#5D00F5]">
+            <span>
+                Os materiais exibidos no site da KDLouvor podem incluir erros técnicos,
+                tipográficos ou fotográficos. KDLouvor não garante que qualquer material em seu site seja preciso,
+                completo ou atual. KDLouvor pode fazer alterações nos materiais contidos em seu site a qualquer momento,
+                sem aviso prévio. No entanto, KDLouvor não se compromete a atualizar os materiais.
+            </span>
+        </p>
+        <br>
+        <h3 class="text-lg font-bold text-[#5D00F5]">
+            <span>6. Links</span>
+        </h3>
+        <br>
+        <p class="text-[#5D00F5]">
+            <span>
+                O KDLouvor não analisou todos os sites vinculados ao seu site e não é
+                responsável pelo conteúdo de nenhum site vinculado. A inclusão de qualquer link não implica endosso por
+                KDLouvor do site. O uso de qualquer site vinculado é por conta e risco do usuário.
+            </span>
+        </p>
+        <br>
+        <h3 class="text-lg font-bold text-[#5D00F5]">
+            <span>Modificações</span>
+        </h3>
+        <br>
+        <p class="text-[#5D00F5]">
+            <span>
+                O KDLouvor pode revisar estes termos de serviço do site a qualquer
+                momento, sem aviso prévio. Ao usar este site, você concorda em ficar vinculado à versão atual desses
+                termos de serviço.
+            </span>
+        </p>
+        <br>
+        <h3 class="text-lg font-bold text-[#5D00F5]">
+            <span>Lei aplicável</span>
+        </h3>
+        <br>
+        <p class="text-[#5D00F5]">
+            <span>
+                Estes termos e condições são regidos e interpretados de acordo com as
+                leis do KDLouvor e você se submete irrevogavelmente à jurisdição exclusiva dos tribunais naquele estado
+                ou localidade.
+            </span>
+        </p>
+    </div>
 </template>
