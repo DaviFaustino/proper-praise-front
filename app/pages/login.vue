ac9564c8ef7e5aea563ef4e3be13bafe1267953c
DaviFaustino <davifaustino.dev@gmail.com>
2025-11-16 18:23:49 -0300

    feat: create login logic

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,15 +1,43 @@
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+import { useRouter } from "vue-router";
+
+const username = ref('');
+const password = ref('');
+
+const config = useRuntimeConfig();
+const backendURL = config.public.backendUrl;
+const { setAccessToken } = useAuth();
+const router = useRouter();
+
+function requestLogin() {
+    axios.post(backendURL + "/api/authentication/login", {}, {
+        headers: {
+            'Authorization': 'Basic ' + btoa(`${username.value}:${password.value}`)
+        }}).then(response => {
+            setAccessToken(response.data.accessToken);
+            localStorage.setItem('refreshToken', response.data.refreshToken);
+
+            router.push('/');
+        }).catch(error => {
+            console.error("There was an error!");
+        });
+}
+</script>
+
 <template>
     <div class="flex flex-col items-center justify-center h-[18rem] w-[32rem] mt-24 bg-white rounded-2xl shadow-2xl">
         <span class="mb-10 text-3xl text-[#5D00F5] font-bold">LOGIN</span>
 
-        <form class="flex flex-col">
+        <form @submit.prevent="requestLogin" class="flex flex-col">
             <div class="flex items-center space-x-2">
-                <label for="" class="text-xl text-[#5D00F5] w-14">Nome</label>
-                <input type="text" class="h-8 w-72 border-2 border-[#5D00F5] rounded">
+                <label class="text-xl text-[#5D00F5] w-14">Nome</label>
+                <input type="text" v-model="username" class="h-8 w-72 p-2 border-2 border-[#5D00F5] rounded">
             </div>
             <div class="flex mt-2 items-center space-x-2">
-                <label for="" class="text-xl text-[#5D00F5] w-14">Senha</label>
-                <input type="text" class="h-8 w-72 border-2 border-[#5D00F5] rounded">
+                <label class="text-xl text-[#5D00F5] w-14">Senha</label>
+                <input type="password" v-model="password" class="h-8 w-72 p-2 border-2 border-[#5D00F5] rounded">
             </div>
 
             <button type="submit" class="h-10 w-32 mt-8 bg-[#5D00F5] text-white rounded self-center">Entrar</button>
