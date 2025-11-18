9686af928511be5c65c2dc08670df5bdbe9aad25
DaviFaustino <davifaustino.dev@gmail.com>
2025-11-18 15:21:43 -0300

    style: add responsive design to the login page

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -27,20 +27,22 @@ function requestLogin() {
 </script>
 
 <template>
-    <div class="flex flex-col items-center justify-center h-[18rem] w-[32rem] mt-24 bg-white rounded-2xl shadow-2xl">
-        <span class="mb-10 text-3xl text-[#5D00F5] font-bold">LOGIN</span>
-
-        <form @submit.prevent="requestLogin" class="flex flex-col">
-            <div class="flex items-center space-x-2">
-                <label class="text-xl text-[#5D00F5] w-14">Nome</label>
-                <input type="text" v-model="username" class="h-8 w-72 p-2 border-2 border-[#5D00F5] rounded">
-            </div>
-            <div class="flex mt-2 items-center space-x-2">
-                <label class="text-xl text-[#5D00F5] w-14">Senha</label>
-                <input type="password" v-model="password" class="h-8 w-72 p-2 border-2 border-[#5D00F5] rounded">
-            </div>
-
-            <button type="submit" class="h-10 w-32 mt-8 bg-[#5D00F5] text-white rounded self-center">Entrar</button>
-        </form>
+    <div class="flex flex-col justify-center flex-grow">
+        <div class="flex flex-col items-center justify-center h-60 md:h-[18rem] w-96 mb-36 mt-20 md:w-[32rem] bg-white rounded-2xl shadow-2xl">
+            <span class="mb-8 md:mb-10 text-2lx md:text-3xl text-[#5D00F5] font-bold">LOGIN</span>
+    
+            <form @submit.prevent="requestLogin" class="flex flex-col text-[#5D00F5]">
+                <div class="flex items-center space-x-2">
+                    <label class="text-lg md:text-xl w-11 md:w-12">Nome</label>
+                    <input type="text" v-model="username" class="h-7 w-64 p-2 border-2 border-[#5D00F5] rounded">
+                </div>
+                <div class="flex mt-2 items-center space-x-2">
+                    <label class="text-lg md:text-xl w-11 md:w-12">Senha</label>
+                    <input type="password" v-model="password" class="h-7 w-64 p-2 border-2 border-[#5D00F5] rounded">
+                </div>
+    
+                <button type="submit" class="h-8 md:h-10 w-28 md:w-32 mt-6 md:mt-8 bg-[#5D00F5] text-white md:text-lg rounded self-center">Entrar</button>
+            </form>
+        </div>
     </div>
 </template>
