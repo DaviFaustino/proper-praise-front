aa2af5be90e7464d3262943b2b4f12fc9b9fc2b5
DaviFaustino <davifaustino.dev@gmail.com>
2026-03-01 10:57:26 -0300

    feat: add ArticleEditingInputField component

---
+++ app/components/ArticleEditingInputField.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="flex flex-col">
+        <label class="font-bold text-sm sm:text-[1rem] mb-1">{{ label }}</label>
+        <input type="text" class="border rounded-md p-1 w-full text-sm sm:text-md focus:outline-none " :class="[ !isValueValid && validationFailed ? 'border-red-500 text-red-500 placeholder-red-500': 'border-gray-300' ]" :placeholder="placeholder" v-model="fieldValue"/>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    label: String,
+    placeholder: String,
+    validationFailed: Boolean,
+    isValueValid: Boolean
+});
+const fieldValue = defineModel();
+</script>
